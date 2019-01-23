ec0466beb0263ab363be97d7878f8d7b3e7abf24
mrblrzl <m.s.arzola@gmail.com>
2019-01-22 22:26:38 -0800

    Updates

---
+++ precache-manifest.d6a225b9dbbf83f30264bd1a938e8424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f15204103e819881647",
+    "url": "/NYT-Google-Books-Search/static/js/runtime~main.3f152041.js"
+  },
+  {
+    "revision": "f0a159e1529c0259f2b1",
+    "url": "/NYT-Google-Books-Search/static/js/main.f0a159e1.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/NYT-Google-Books-Search/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f0a159e1529c0259f2b1",
+    "url": "/NYT-Google-Books-Search/static/css/main.141739e9.chunk.css"
+  },
+  {
+    "revision": "01d1e58517fcd5d20683df982ac4b7cc",
+    "url": "/NYT-Google-Books-Search/index.html"
+  }
+];
